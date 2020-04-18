7c0e52c11681d60f518528013368d18b887e9f55
Damian <damian.ogorek.1996@gmail.com>
2020-04-18 17:54:50 +0200

    Implemented repository into view model.

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -38,20 +38,20 @@ interface VideoDao {
 @Database(entities = [VideoDao::class], version = 1)
 abstract class VideosDatabase : RoomDatabase() {
     abstract val videoDao: VideoDao
+}
 
-    private lateinit var INSTANCE: VideosDatabase
+private lateinit var INSTANCE: VideosDatabase
 
-    fun getDatabase(context: Context): VideosDatabase {
-        synchronized(VideosDatabase::class.java) {
-            if (!::INSTANCE.isInitialized) {
-                INSTANCE = Room.databaseBuilder(
-                                context.applicationContext,
-                                VideosDatabase::class.java,
-                                "videos")
-                        .build()
-            }
-
-            return INSTANCE
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            VideosDatabase::class.java,
+                            "videos")
+                    .build()
         }
+
+        return INSTANCE
     }
 }
